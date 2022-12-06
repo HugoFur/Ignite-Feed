f451e85fde8b36142ac0be6e1ec3c06694c394bb
Hugo_Furtado <71742937+HugoFur@users.noreply.github.com>
2022-12-06 15:38:23 -0300

    :sparkles: feat(componente Post): criação estrutura inicial de Post

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,15 @@ import { Post } from "./Post";
 
 export function App() {
   return(
-    <Post />
+    <div>
+      <Post 
+      author="Hugo Furtado"
+      content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi aliquid nisi dignissimos, quasi omnis sapiente!"
+      />
+      <Post 
+      author="Outro usuário"
+      content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi aliquid nisi dignissimos, quasi omnis sapiente!"
+      />
+    </div>
   )
 }
