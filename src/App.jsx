52c4c6c7073d5b08654b402255fb159d87a8cb29
Hugo_Furtado <71742937+HugoFur@users.noreply.github.com>
2022-12-06 17:30:49 -0300

    :bricks: feat(componente): criação e estilização de Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { Post } from "./Post";
+import { Post } from "./components/Post";
+import { Header } from "./components/Header"
 
 import "./global.css"
 
 export function App() {
   return(
     <div>
+      <Header />
       <Post 
       author="Hugo Furtado"
       content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi aliquid nisi dignissimos, quasi omnis sapiente!"
