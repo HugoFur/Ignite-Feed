56aae439c04c45ffff3bb048a75fdc1892c12ce0
Hugo_Furtado <71742937+HugoFur@users.noreply.github.com>
2022-12-08 13:32:50 -0300

    :bricks: feat(componente): estrutura HTML do componente Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,31 @@
-export function Post(props){
+import styles from "./Post.module.css"
+
+export function Post(){
     return (
-        <div>
-            <strong> { props.author } </strong>
-            <p> { props.content } </p>
-        </div>
+        <article className={styles.post}>
+            
+            <header>
+                <div className={styles.author}>
+                    <img 
+                    className={styles.avatar}
+                    src="https://avatars.githubusercontent.com/u/71742937?v=4"
+                    alt="Foto de perfil do usuário"
+                    />
+                    <div className={styles.authorInfo}>
+                        <strong>Hugo Furtado</strong>
+                        <span>Web developer</span>
+                    </div>
+                </div>
+
+                <time dateTime="2022-12-08 11:10:15">Publicado há 1h</time>
+            </header>
+        
+            <div className={styles.content}>
+                <p>Fala camaradas 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifolio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare</p>
+                <p><a href="">site.com</a></p>
+                <p><a href="">#novoprojeto #RocketSeat</a></p>
+            </div>
+        </article>
     )
 }
