fc12872154f8db6c025b31d5443dd7054faf6333
Hugo_Furtado <71742937+HugoFur@users.noreply.github.com>
2022-12-10 15:31:16 -0300

    feat(estrtura inicial): Criada estrutura de comentarios

---
+++ src/components/Comment.jsx
@@ -0,0 +1,31 @@
+import { ThumbsUp, Trash } from "phosphor-react"
+import styles from "./Comment.module.css"
+
+export function Comment() {
+    return(
+        <div className={styles.comment}>
+            <img src="https://github.com/HugoFur.png" alt="" />
+            
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Hugo Furtado</strong>
+                            <time dateTime="2022-12-08 11:10:15">Cerca de 1h atrás</time>
+                        </div>
+                        <button title="Deletar comentarios">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+                    <p>Parabéns, o site ficou otimo!</p>
+                </div>
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>21</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+} 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import igniteLogo from '../assets/ignite-logo.svg'
 export function Header() {
   return (
     <header className={styles.header}>
-      <img src={igniteLogo} alt="Logotipo da aplicação web: Ignite" />
+      <img src={igniteLogo} alt="Logotipo da aplicação web: Ignite Feed" />
     </header>
   );
 }
