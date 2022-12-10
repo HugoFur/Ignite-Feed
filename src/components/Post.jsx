c21db178bb26d306daf75e52fdd1fcdedcbee234
Hugo_Furtado <71742937+HugoFur@users.noreply.github.com>
2022-12-10 12:30:36 -0300

    feat(criação comentarios): adição de seção de comentarios em componente post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -32,6 +32,16 @@ export function Post(){
                     <a href="">#devFrontend</a>
                 </p>
             </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+                <textarea
+                placeholder="Deixe um comentario..." 
+                />
+                <footer>
+                    <button type="submit">Publicar</button>
+                </footer>
+            </form>
         </article>
     )
 }
