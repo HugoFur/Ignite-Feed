0b00af009dfd2ed05d56cda628ccef84ab645d6d
Hugo_Furtado <71742937+HugoFur@users.noreply.github.com>
2022-12-12 15:01:17 -0300

    refactor(componente Post): criação de iteração e data formatada

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,37 @@ import { Sidebar} from "./components/Sidebar"
 import styles from "./App.module.css"
 import "./global.css"
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/HugoFur.png",
+      name: "Hugo Furtado",
+      role: "Web developer"
+    },
+    content: [
+      { type: "paragraph", content:"Fala camaradas 👋"},
+      {type: "paragraph", content:"Acabei de subir esse projeto frontend no meu portifolio. É um projeto que fiz durante as aulas do módulo de React da RocketSeat"},
+      {type:"link", content:"https://github.com/HugoFur"}
+    ],
+    publishedAt: new Date("2022-12-12 12:00:00")
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/HugoFur.png",
+      name: "Hugo Furtado",
+      role: "Web developer"
+    },
+    content: [
+      { type: "paragraph", content:"Fala camaradas 👋"},
+      {type: "paragraph", content:"Acabei de subir esse projeto frontend no meu portifolio. É um projeto que fiz durante as aulas do módulo de React da RocketSeat"},
+      {type:"link", content:"https://github.com/HugoFur"}
+    ],
+    publishedAt: new Date("2022-12-12 12:00:00")
+  },
+];
+
 export function App() {
   return(
     <div>
@@ -13,14 +44,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
        <main>
-        <Post 
-        author="Hugo Furtado"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi aliquid nisi dignissimos, quasi omnis sapiente!"
-        />
-        <Post 
-        author="Hugo Furtado"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi aliquid nisi dignissimos, quasi omnis sapiente!"
-        />
+        {posts.map(post =>{
+          return (
+          <Post
+            author={post.author}
+            content={post.content}
+            publishedAt={post.publishedAt}
+          />
+          )
+        })}
        </main>
       </div>
     </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,34 +1,43 @@
 import { Avatar } from "./Avatar"
 import { Comment } from "./Comment"
 import styles from "./Post.module.css"
+import { format, formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR';
 
-export function Post() {
+export function Post({ author, publishedAt, content }) {
+    const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+        locale: ptBR,
+    });
+
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale: ptBR,
+        addSuffix: true
+    });
     return (
         <article className={styles.post}>
 
             <header>
                 <div className={styles.author}>
-                    <Avatar src="https://avatars.githubusercontent.com/u/71742937?v=4" />
+                    <Avatar src="https://github.com/HugoFur.png" />
                     <div className={styles.authorInfo}>
-                        <strong>Hugo Furtado</strong>
-                        <span>Web developer</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
 
-                <time dateTime="2022-12-08 11:10:15">Publicado há 1h</time>
+                <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+                    {publishedDateRelativeToNow}
+                </time>
             </header>
 
             <div className={styles.content}>
-                <p>Fala camaradas 👋</p>
-                <p>Acabei de subir mais um projeto no meu portifolio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-                <p>
-                    <a href="">site.com</a>
-                </p>
-                <p>
-                    <a href="">#novoprojeto</a>{'  '}
-                    <a href="">#RocketSeat</a>{'  '}
-                    <a href="">#devFrontend</a>
-                </p>
+                {content.map(line => {
+                    if (line.type === 'paragraph') {
+                        return <p>{line.content}</p>;
+                    } else if (line.type === 'link') {
+                        return <p><a href="#">{line.content}</a></p>
+                    }
+                })}
             </div>
 
             <form className={styles.commentForm}>
@@ -42,8 +51,6 @@ export function Post() {
             </form>
 
             <div className={styles.commentList}>
-                <Comment />
-                <Comment />
                 <Comment />
             </div>
         </article>
